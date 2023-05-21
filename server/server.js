20d9042ae748f7b3e8fb9400abeb63d1c6c9d04e
Beto Lopez <363alberto@gmail.com>
2023-05-21 13:34:12 -0400

    set up Mongo Database

--- server/server.js
+++ server/server.js
@@ -3,12 +3,19 @@ import express from 'express'
 import devBundle from './devBundle'
 import path from 'path'
 import template from './../template'
+import { MongoClient } from 'mongodb'
+
+const url = process.env.MONGODB_URL || 'mongodb://localhost:27017/mernSimpleSetup'
 let port = process.env.PORT || 3000
 
 const app = express()
 const CURRENT_WORKING_DIR = process.cwd()
 //comment out when in production
 devBundle.compile(app)
+MongoClient.connect(url, (err,db) =>{
+    console.log("Connected succesfully to mongodb server")
+    db.close()
+})
 
 app.get('/', (req,res) => {
     res.status(200).send(template())
