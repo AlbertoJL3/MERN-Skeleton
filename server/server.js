585ae89425d70901bacd1a6bbb0340a15d4d57de
Beto Lopez <363alberto@gmail.com>
2023-05-21 12:51:41 -0400

    added comments

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import express from 'express'
+//comment out when in production
 import devBundle from './devBundle'
 
 
 const app = express()
 
+//comment out when in production
 devBundle.compile(app)
