4279f224ba7329b8ca4e0b7f09b9427e9a32f5b2
Beto Lopez <363alberto@gmail.com>
2023-05-21 12:07:36 -0400

    added webpack.config.server configuration

--- webpack.config.client.production.js
+++ webpack.config.client.production.js
@@ -2,6 +2,7 @@ const path = require('path')
 const webpack = require('webpack')
 const CURRENT_WORKING_DIR = process.cwd()
 
-const config = {...CURRENT_WORKING_DIR}
+const config = {
+}
 
 module.exports = config
